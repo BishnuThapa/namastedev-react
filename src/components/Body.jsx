7e3556c8696b13943f6c6075cda25f7026c6aab3
Bishnu <thapabishnu20@gmail.com>
2024-07-04 14:49:26 +0545

    filter restaurant list with more than 4 stars on button click

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import RestaurantCard from './RestaurantCard';
 import resList from '../utils/mockData';
 
 const Body = () => {
+  const [listOfRestaurants,setListOfRestaurants] = useState(resList);
   return (
     <div className="body">
-      <div className="search">Search</div>
+      {/* <div className="search">Search</div> */}
+      <div className="filter">
+        <button className='filter-btn' onClick={() => {
+          const filteredList=listOfRestaurants.filter(res=>res.data.avgRating >4)
+          setListOfRestaurants(filteredList)
+        }}>Top Rated Restaurant</button>
+      </div>
       <div className="res-container">
         {/* Restro card Container */}
-        {resList.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.data.id} resData={restaurant} />
         ))}
 
