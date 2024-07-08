ec607b3315de780fc8093e0c7d8f1ce7455ea2c5
Bishnu <thapabishnu20@gmail.com>
2024-07-08 14:11:01 +0545

    resolve new swiggy api

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,22 +1,43 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import RestaurantCard from './RestaurantCard';
 import resList from '../utils/mockData';
 
 const Body = () => {
-  const [listOfRestaurants,setListOfRestaurants] = useState(resList);
+  // const [listOfRestaurants,setListOfRestaurants] = useState(resList);
+  const [listOfRestaurants,setListOfRestaurants] = useState([]);
+
+  useEffect(() => { fetchData() }, []);
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.960059122809971&lng=77.57337538383284&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json()
+    console.log(json)
+    setListOfRestaurants(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+    .restaurants);
+  }
+
   return (
     <div className="body">
       {/* <div className="search">Search</div> */}
       <div className="filter">
-        <button className='filter-btn' onClick={() => {
-          const filteredList=listOfRestaurants.filter(res=>res.data.avgRating >4)
-          setListOfRestaurants(filteredList)
-        }}>Top Rated Restaurant</button>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.data.avgRating > 4
+            );
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurant
+        </button>
       </div>
       <div className="res-container">
         {/* Restro card Container */}
         {listOfRestaurants.map((restaurant) => (
-          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
 
         {/* LIMIT RESULTS ON FETCH { resList.slice(0,5).map(restaurant=><RestaurantCard resData={restaurant} />)} */}
--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -8,23 +8,20 @@ import { CDN_URL } from '../utils/constants';
      avgRating,
      cuisines,
      costForTwo,
-     deliveryTime,
-   } = resData?.data;
+     sla,
+   } = resData?.info;
    return (
      <div className="res-card">
        <img
          className="res-image"
-         src={CDN_URL
-            +
-           cloudinaryImageId
-         }
+         src={CDN_URL + cloudinaryImageId}
          alt="test"
        />
        <h3>{name}</h3>
        <h4>{cuisines.join(",")}</h4>
        <h4>{avgRating} stars</h4>
-       <h4>{costForTwo / 100} FOR TWO.</h4>
-       <h4>{deliveryTime} min</h4>
+       <h4>{costForTwo}</h4>
+       <h4>{sla.slaString}</h4>
      </div>
    );
  };
