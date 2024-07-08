d58664c9240ee18af85afb189d1534c338e6311d
Bishnu <thapabishnu20@gmail.com>
2024-07-08 15:36:43 +0545

    add cors proxy to swiggy url

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -9,10 +9,10 @@ const Body = () => {
   useEffect(() => { fetchData() }, []);
   const fetchData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.960059122809971&lng=77.57337538383284&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.960059122809971&lng=77.57337538383284&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json()
-    console.log(json)
+    // console.log(json)
     setListOfRestaurants(
       json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
     .restaurants);
